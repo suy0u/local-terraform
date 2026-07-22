a45027272e0cbdf4b4428effd22caca3d54d364f
suy0u <proton-coy3p@icloud.com>
2026-07-22 22:32:35 +0200

    lesson 13: use lookup() maps for instance_type, add test sg

--- 13-lookups-and-conditions/main.tf
+++ 13-lookups-and-conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "webserver1" {
   ami           = "ami-024f768332f0"
-  instance_type = var.env == "prod" ? "t2.large" : "t2.micro"
+  instance_type = var.env == "prod" ? var.ec2_size[var.env] : var.ec2_size["dev"]
 
   tags = {
     Name  = "${var.env}-server"
@@ -16,5 +16,36 @@ resource "aws_instance" "bastion" {
   tags = {
     Name  = "${var.env}-server"
     Owner = var.env == "prod" ? var.prod_owner : var.nonprod_owner 
+  }
+}
+
+resource "aws_instance" "webserver2" {
+  ami           = "ami-024f768332f0"
+  instance_type = lookup(var.ec2_size, var.env)
+
+  tags = {
+    Name  = "${var.env}-server"
+    Owner = var.env == "prod" ? var.prod_owner : var.nonprod_owner 
+  }
+}
+
+resource "aws_security_group" "webserver_sg" {
+  name = "Web Server Security Group"
+
+  dynamic "ingress" {
+    for_each = lookup(var.allow_port_list, var.env) 
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- 13-lookups-and-conditions/variables.tf
+++ 13-lookups-and-conditions/variables.tf
@@ -9,3 +9,18 @@ variable "prod_owner" {
 variable "nonprod_owner" {
   default = "noname"
 }
+
+variable "ec2_size" {
+  default = {
+    "prod"    = "t3.medium"
+    "dev"     = "t3.micro"
+    "staging" = "t2.small"
+  }
+}
+
+variable "allow_port_list" {
+  default = {
+    "prod" = ["80", "443"]
+    "dev"  = ["80", "443", "8080", "22"]
+  }
+}
